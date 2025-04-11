96f33cae012ebc125c6b364196036ed8571930ad
Abe Coutinho <abrcoutinho@gmail.com>
2025-04-10 23:00:37 -0300

    06

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,4 @@
 
-// User floating menu
-const userButton = document.querySelector('.header .user button')
-const userFloatingMenu = document.querySelector('.user-floating-menu');
-
-function showUserFloatingMenu() {
-  userFloatingMenu.classList.add('visible')
-}
-window.addEventListener('click', function(e){
-  if (!userFloatingMenu.contains(e.target) && !userButton.contains(e.target)) {
-    userFloatingMenu.classList.remove("visible");
-  }
-});
-
 
 // Account tabs
 function accountForms(evt, formName) {
@@ -60,14 +47,17 @@ function instructions() {
 // popup history
 const popupHistory = document.querySelector('.popup-history');
 const labelChange = document.getElementById('labelChange');
+const labelChange2 = document.getElementById('labelChange2');
 
 function historyPopup() {
   popupHistory.classList.add('visible');
   labelChange.textContent="Histórico";
+  labelChange2.textContent="Histórico";
 }
 function closeHistory() {
   popupHistory.classList.remove('visible');
   labelChange.textContent="Consulta";
+  labelChange2.textContent="Paciente";
 }
 
 
@@ -89,3 +79,47 @@ function historyTabs(evt, historyStatus) {
   document.getElementById(historyStatus).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+
+// Patient list
+const patientSelected = document.querySelector('.patient-selected');
+const patientList = document.querySelector('.patients-list');
+
+function mobPatientList() {
+  patientList.classList.add('visible');
+}
+
+// User floating menu
+const userButton = document.querySelector('.header .user button')
+const userFloatingMenu = document.querySelector('.user-floating-menu');
+
+function showUserFloatingMenu() {
+  userFloatingMenu.classList.add('visible')
+}
+
+// Notification popup
+const bellButton = document.querySelector('.header-account .notification button')
+const popupNotif = document.querySelector('.popup-notifications');
+
+function showPopupNotifications() {
+  popupNotif.classList.add('visible')
+}
+
+
+window.addEventListener('click', function(e){
+  if (!userFloatingMenu.contains(e.target) && !userButton.contains(e.target)) {
+    userFloatingMenu.classList.remove("visible");
+  }
+
+  if (!bellButton.contains(e.target) && !popupNotif.contains(e.target)) {
+    popupNotif.classList.remove("visible");
+  }
+
+  if (!patientList.contains(e.target) && !patientSelected.contains(e.target)) {
+    patientList.classList.remove("visible");
+  }
+});
+
+
+
+
